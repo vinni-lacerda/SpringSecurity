3e322bb1fe52453bceccc6a164f83fe29421b5ba
vinni-lacerda <vini.lacerda@hotmail.com>
2026-03-12 22:31:13 -0300

    feat: First commit :D

---
+++ src/test/java/study/spring_security/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
