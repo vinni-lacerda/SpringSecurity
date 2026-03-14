8683f2e1ae94762132b69f322c97348e5240331c
vinni-lacerda <vini.lacerda@hotmail.com>
2026-03-14 01:33:00 -0300

    feat: implementing StartApplication

---
+++ src/main/java/study/spring_security/init/StartApplication.java
@@ -0,0 +1,35 @@
+package study.spring_security.init;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import study.spring_security.model.User;
+import study.spring_security.repository.UserRepository;
+
+@Component
+public class StartApplication implements CommandLineRunner {
+    @Autowired
+    private UserRepository repository;
+    @Transactional
+    @Override
+    public void run(String... args) throws Exception {
+        User user = repository.findByUsername("admin");
+        if(user==null){
+            user = new User();
+            user.setName("ADMIN");
+            user.setUsername("admin");
+            user.setPassword("master123");
+            user.getRoles().add("MANAGERS");
+            repository.save(user);
+        }
+        user = repository.findByUsername("user");
+        if(user ==null){
+            user = new User();
+            user.setName("USER");
+            user.setUsername("user");
+            user.setPassword("user123");
+            user.getRoles().add("USERS");
+            repository.save(user);
+        }
+    }
+}
