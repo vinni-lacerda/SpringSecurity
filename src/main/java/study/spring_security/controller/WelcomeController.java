49ddf7a1982870d4c9ed3574ec4e3a151a0be4c0
vinni-lacerda <vini.lacerda@hotmail.com>
2026-03-14 01:35:43 -0300

    refactor: moving controller to right package

--- src/main/java/study/spring_security/controller/WelcomeController.java
+++ src/main/java/study/spring_security/controller/WelcomeController.java
@@ -1,6 +1,5 @@
-package study.spring_security;
+package study.spring_security.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
